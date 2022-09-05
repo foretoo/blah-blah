1c9489baac30e9e03d1412647dbd7bece5c5b9c8
Sergey Borovikov <serg.there@gmail.com>
2022-09-05 21:20:41 +0300

    fix: correct name

--- src/shared/snoise4D.glsl
+++ src/shared/snoise4D.glsl
@@ -18,7 +18,7 @@ vec4 grad4(float j, vec4 ip){
   return p;
 }
 
-float snoise(vec4 v){
+float snoise4D(vec4 v){
   const vec2  C = vec2( 0.138196601125010504,  // (5 - sqrt(5))/20  G4
                         0.309016994374947451); // (sqrt(5) - 1)/4   F4
   // First corner
