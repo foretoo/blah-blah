6a8191d909836357540a363df9cf9beff72efb97
Sergey Borovikov <serg.there@gmail.com>
2022-09-05 21:56:28 +0300

    add rough3D (random point inside unit sphere)

--- src/shared/rough.glsl
+++ src/shared/rough.glsl
@@ -1,7 +1,21 @@
-float random (vec2 st) {
-  return fract( sin(dot(st.xy, vec2(12.9898, 78.233))) * 43758.5453123 );
-}
+#include random;
 
 float rough (vec2 st) {
   return (random(st) - 0.5) * 2.0;
+}
+
+vec3 rough3D (vec3 point) {
+  float u = random(point.xy);
+  float v = random(point.yz);
+  float theta = u * 2.0 * 3.14159265;
+  float phi = acos(2.0 * v - 1.0);
+  float r = pow(random(point.zx), 1.0/3.0);
+  float sinTheta = sin(theta);
+  float cosTheta = cos(theta);
+  float sinPhi = sin(phi);
+  float cosPhi = cos(phi);
+  float x = r * sinPhi * cosTheta;
+  float y = r * sinPhi * sinTheta;
+  float z = r * cosPhi;
+  return vec3(x, y, z);
 }
