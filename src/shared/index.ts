79a5417ff5b77c096d926bd30a8c7a12f1e2d14d
Sergey Borovikov <serg.there@gmail.com>
2022-09-05 20:40:27 +0300

    shared refactored

--- src/shared/index.ts
+++ src/shared/index.ts
@@ -1,4 +1,5 @@
 export { default as cnoise } from "./cnoise.glsl"
-export { default as snoise } from "./snoise.glsl"
+export { default as snoise3D } from "./snoise3D.glsl"
+export { default as snoise4D } from "./snoise4D.glsl"
 
 export { spherePointsAmount, spherePositions } from "./sphereBuffer"
