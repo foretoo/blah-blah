638ceae31aaa21f77c2e25e010daa252424ef8a2
Sergey Borovikov <serg.there@gmail.com>
2022-09-10 15:59:34 +0300

    add z dim

--- src/pointer.ts
+++ src/pointer.ts
@@ -15,6 +15,8 @@ export const initiatePointer = (
   addEventListener("pointermove", (e) => {
     pointer.x = (e.clientX / innerWidth  *  2 - 1) * camera.right * (1 / camera.zoom)
     pointer.y = (e.clientY / innerHeight * -2 + 1) * (1 / camera.zoom)
+    const clen = Math.sqrt(pointer.x ** 2 + pointer.y ** 2)
+    pointer.z = clen < 1 ? Math.cos(clen * Math.PI / 2) : 0
 
     const [ dx, dy ] = [ pointer.x - prevPointer.x, pointer.y - prevPointer.y ]
     pointer.d += Math.sqrt(dx ** 2 + dy ** 2)
