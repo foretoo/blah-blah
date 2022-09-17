7a05c516e269d109d2c092cdce1d551083eb8109
Sergey Borovikov <serg.there@gmail.com>
2022-09-17 14:19:25 +0300

    add platonic sdf's

--- src/sphere/shaders/compute-noise.glsl
+++ src/sphere/shaders/compute-noise.glsl
@@ -12,7 +12,52 @@ uniform float roughness;
 #include ../../shared/cnoise;
 #include ../../shared/rough3D;
 
-const float PI = 3.14159265359;
+#define PI  3.14159265359
+#define PHI 1.61803398875
+
+
+
+float tetr(vec3 pos, float r) {
+  pos.xz = abs(pos.xz);
+  vec3 n = normalize(vec3(0.0, sqrt(0.5), 1.0));
+  float d = max(dot(pos, n.xyz), dot(pos, n.zyx * vec3(1.0, -1.0, 1.0)));
+  return pow(pow(d, 1.0/100.0) - r, 100.0);
+}
+
+float oct(vec3 pos, float r) {
+  pos = abs(pos);
+  vec3 n = normalize(vec3(1.0));
+  float d = dot(pos, n);
+  return pow(pow(d, 1.0/100.0) - r, 100.0);
+}
+
+float cube(vec3 pos, float r) {
+  pos = abs(pos);
+  vec3 n = vec3(1.0, 0.0, 0.0);
+  float d =    dot(pos, n.xyz);
+    d = max(d, dot(pos, n.zxy));
+    d = max(d, dot(pos, n.yzx));
+  return pow(pow(d, 1.0/100.0) - r, 100.0);
+}
+
+float dodec(vec3 pos, float r){
+  pos = abs(pos);
+	vec3 n = normalize(vec3(PHI, 1.0, 0.0));
+	float d =    dot(pos, n.xyz);
+    d = max(d, dot(pos, n.yzx));
+    d = max(d, dot(pos, n.zxy));
+	return pow(pow(d, 1.0/100.0) - r, 100.0);
+}
+
+float icos(vec3 pos, float r) {
+  pos = abs(pos);
+  vec3 n = normalize(vec3(0, 1.0 / PHI, PHI));
+  float d =    dot(pos, normalize(vec3(1)));
+    d = max(d, dot(pos, n.xyz));
+    d = max(d, dot(pos, n.yzx));
+    d = max(d, dot(pos, n.zxy));
+  return pow(pow(d, 1.0/100.0) - r, 100.0);
+}
 
 
 
@@ -27,7 +72,8 @@ void main() {
   float rlen = length(rpos);
   rpos *= rlen * roughness;
 
-  vec3 cpos = cnoise((position + rpos) * noiseScale + t) * sphereScale;
+  vec3 cpos = cnoise((position + rpos) * noiseScale + t);
+  cpos *= cube(cpos, 2.0) * sphereScale;
 
   // float n = snoise(position + t) * 0.5 + 0.5;
   // vec3 pos = mix(position, cpos, 1.0 - n * 0.1);
