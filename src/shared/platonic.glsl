f378657d9a30908a8bbed1ec0105976315658d96
Sergey Borovikov <serg.there@gmail.com>
2022-09-23 17:08:40 +0300

    fix: stretching, del pows

--- src/shared/platonic.glsl
+++ src/shared/platonic.glsl
@@ -1,23 +1,35 @@
 #define PHI 1.61803398875
 
+const int tetr_factor = 8;
+const int oct_factor = 4;
+const int cube_factor = 6;
+const int dodec_factor = 4;
+const int icos_factor = 4;
+
 
 
 float tetr(vec3 pos, float r) {
   pos.xz = abs(pos.xz);
   vec3 n = normalize(vec3(0.0, sqrt(0.5), 1.0));
   float d = max(dot(pos, n.xyz), dot(pos, n.zyx * vec3(1.0, -1.0, 1.0)));
-    d = pow(pow(d, 1.0/100.0) - r, 100.0) * 0.618;
-  float result = d * isTetra + 1.0 - isTetra;
-  return result;
+
+  for (int i = 0; i < tetr_factor; i++) d = sqrt(d);
+    d -= 2.0;
+  for (int i = 0; i < tetr_factor; i++) d = d * d;
+
+  return d * isTetra + 1.0 - isTetra;
 }
 
 float oct(vec3 pos, float r) {
   pos = abs(pos);
   vec3 n = normalize(vec3(1.0));
   float d = dot(pos, n);
-    d = pow(pow(d, 1.0/100.0) - r, 100.0) * 0.8;
-  float result = d * isOcta + 1.0 - isOcta;
-  return result;
+
+  for (int i = 0; i < oct_factor; i++) d = sqrt(d);
+    d -= 2.0;
+  for (int i = 0; i < oct_factor; i++) d = d * d;
+
+  return d * isOcta + 1.0 - isOcta;
 }
 
 float cube(vec3 pos, float r) {
@@ -26,9 +38,12 @@ float cube(vec3 pos, float r) {
   float d =    dot(pos, n.xyz);
     d = max(d, dot(pos, n.zxy));
     d = max(d, dot(pos, n.yzx));
-    d = pow(pow(d, 1.0/100.0) - r, 100.0) * 0.9;
-  float result = d * isCube + 1.0 - isCube;
-  return result;
+  
+  for (int i = 0; i < cube_factor; i++) d = sqrt(d);
+    d -= 2.0;
+  for (int i = 0; i < cube_factor; i++) d = d * d;
+
+  return d * isCube + 1.0 - isCube;
 }
 
 float dodec(vec3 pos, float r){
@@ -37,9 +52,12 @@ float dodec(vec3 pos, float r){
 	float d =    dot(pos, n.xyz);
     d = max(d, dot(pos, n.yzx));
     d = max(d, dot(pos, n.zxy));
-	  d = pow(pow(d, 1.0/100.0) - r, 100.0);
-  float result = d * isDodeca + 1.0 - isDodeca;
-  return result;
+
+  for (int i = 0; i < dodec_factor; i++) d = sqrt(d);
+    d -= 2.0;
+  for (int i = 0; i < dodec_factor; i++) d = d * d;
+
+  return d * isDodeca + 1.0 - isDodeca;
 }
 
 float icos(vec3 pos, float r) {
@@ -49,17 +67,20 @@ float icos(vec3 pos, float r) {
     d = max(d, dot(pos, n.xyz));
     d = max(d, dot(pos, n.yzx));
     d = max(d, dot(pos, n.zxy));
-    d = pow(pow(d, 1.0/100.0) - r, 100.0);
-  float result = d * isIcosa + 1.0 - isIcosa;
-  return result;
+
+  for (int i = 0; i < icos_factor; i++) d = sqrt(d);
+    d -= 2.0;
+  for (int i = 0; i < icos_factor; i++) d = d * d;
+
+  return d * isIcosa + 1.0 - isIcosa;
 }
 
 
 
 vec3 platonic(vec3 pos, float r) {
-  pos *= tetr(pos, 2.0);
-  pos *= oct(pos, 2.0);
-  pos *= cube(pos, 2.0);
+  pos *= tetr(pos, 2.0) * 0.618;
+  pos *= oct(pos, 2.0)  * 0.8;
+  pos *= cube(pos, 2.0) * 0.9;
   pos *= dodec(pos, 2.0);
   pos *= icos(pos, 2.0);
   return pos;
