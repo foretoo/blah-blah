a280f200ebadd783441f39201ecbb0203805d847
Sergey Borovikov <serg.there@gmail.com>
2022-10-07 01:35:29 +0300

    config for pkg build

--- vite.config.js
+++ vite.config.js
@@ -5,21 +5,27 @@ export default defineConfig({
   publicDir: false,
   plugins: [glsl()],
   server: {
-    open: "/src/index.html",
+    open: "/pkg/index.html",
     host: true,
   },
-  optimizeDeps: {
-    entries: "./src/index.ts"
-  },
+  optimizeDeps: { entries: "./pkg/index.ts" },
   build: {
-    assetsDir: ".",
-    rollupOptions: {
-      input: "/src/index.ts",
-      output: {
-        dir: "dist",
-        assetFileNames: "style.css",
-        entryFileNames: "bundle.js",
-      },
+    // assetsDir: ".",
+    // rollupOptions: {
+    //   input: "/src/index.ts",
+    //   output: {
+    //     dir: "dist",
+    //     assetFileNames: "style.css",
+    //     entryFileNames: "bundle.js",
+    //   },
+    // },
+    lib: {
+      entry: "./pkg/index.ts",
+      fileName: "index",
+      formats: [ "es" ],
     },
+    outDir: "build",
+    rollupOptions: { external: "three" },
+    emptyOutDir: false,
   },
 })
